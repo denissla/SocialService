eddc4c868c323c10efdb9fcc4fafa07e121ac008
Леонов Денис <deniss46leonov@gmail.com>
2022-04-16 14:34:44 +0300

    Добавьте файлы проекта.

---
+++ SocialService/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SocialService.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
+
